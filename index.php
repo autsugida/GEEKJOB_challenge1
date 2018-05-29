e653c88d1746dfcfb7cd2d58ce7a388e13a073e5
UNLIMITED-PC\晃 <ancient725kingdom@gmail.com>
2018-05-29 15:03:23 +0900

    インフラ課題初級

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+echo "雑草魂";
+
+echo "<br>";
+
+//計算結果を$keisan
+
+$keisan=1*2;
+echo $keisan;
+echo "<br>";
+
+$num=rand(0,1,2,3);
+
+if($num==1){
+ echo"大吉";
+}else if($num==0){
+ echo"中吉";
+}else if($num==2){
+ echo"吉";
+}else if($num==3){
+ echo "凶";
+}
+
+?>
